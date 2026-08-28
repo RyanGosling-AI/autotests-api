bc225120717b39f07210bfb641325517f0257fdc
RyanGosling-AI <damnedwretchofficial@gmail.com>
2026-08-28 14:27:16 +1000

    Практикуемся в реализации API клиентов

---
+++ httpx_create_file.py
@@ -0,0 +1,34 @@
+import httpx
+from tools.fakers import get_random_email
+
+create_user_payload = {
+    "email": get_random_email(),
+    "password": "string",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string"
+}
+with httpx.Client(base_url="http://localhost:8000") as client:
+    create_user_response = client.post("/api/v1/users", json=create_user_payload)
+    create_user_response_data = create_user_response.json()
+    print(f'Create user data: {create_user_response_data}')
+
+    login_payload = {
+        "email": create_user_payload["email"],
+        "password": create_user_payload["password"],
+    }
+    login_response = client.post("/api/v1/authentication/login", json=login_payload)
+    login_response_data = login_response.json()
+    print(f'Login data: {login_response_data}')
+
+    create_file_headers = {
+        "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+    }
+    create_file_response = client.post(
+        "/api/v1/files",
+        data={'filename': 'image.jpg', 'directory': 'courses'},
+        files={'upload_file': open('./test_data/files/api_image.png', 'rb')},
+        headers=create_file_headers
+    )
+    create_file_response_data = create_file_response.json()
+    print(f'Create file data: {create_file_response_data}')
