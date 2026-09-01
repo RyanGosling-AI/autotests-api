db303e90dabd9ff3085cb29be2fff5c799e1e347
RyanGosling-AI <damnedwretchofficial@gmail.com>
2026-09-01 17:01:20 +1000

    Валидация JSON schema

--- autotests_api/pydantic_json_schema_get_user.py
+++ autotests_api/pydantic_json_schema_get_user.py
@@ -1,8 +1,9 @@
-from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.private_users_client import get_private_users_client
 from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
+from clients.private_http_builder import AuthenticationUserSchema
 from tools.fakers import get_random_email
+from tools.assertions.schema import validate_json_schema
 
 public_users_client = get_public_users_client()
 
@@ -14,7 +15,7 @@ create_user_request = CreateUserRequestSchema(
     middle_name="string"
 )
 create_user_response = public_users_client.create_user(create_user_request)
-print('Create user data:', create_user_response)
+print("Create user data:", create_user_response)
 
 authentication_user = AuthenticationUserSchema(
     email=create_user_request.email,
@@ -22,7 +23,7 @@ authentication_user = AuthenticationUserSchema(
 )
 private_users_client = get_private_users_client(authentication_user)
 
-get_user_response = private_users_client.get_user(create_user_response.user.id)
-print('Get user data:', get_user_response)
+get_user_response = private_users_client.get_user_api(create_user_response.user.id)
+print("Get user data:", get_user_response)
 
-
+validate_json_schema(instance=get_user_response.json(), schema=GetUserResponseSchema.model_json_schema())
