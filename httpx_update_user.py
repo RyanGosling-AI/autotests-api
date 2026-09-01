72d45a89328a63cd97286552a477689d36ce2ad4
RyanGosling-AI <damnedwretchofficial@gmail.com>
2026-09-01 19:20:16 +1000

    Знакомство с Faker

--- httpx_update_user.py
+++ httpx_update_user.py
@@ -1,8 +1,8 @@
 import httpx
-from tools.fakers import get_random_email
+from tools.fakers import fake
 
 create_user_payload = {
-    "email": get_random_email(),
+    "email": fake.email(),
     "password": "string",
     "lastName": "string",
     "firstName": "string",
@@ -22,7 +22,7 @@ with httpx.Client(base_url="http://localhost:8000") as client:
     print(f'Login data: {login_response_data}')
 
     path_payload = {
-        "email": get_random_email(),
+        "email": fake.email(),
         "lastName": "string",
         "firstName": "string",
         "middleName": "string"
