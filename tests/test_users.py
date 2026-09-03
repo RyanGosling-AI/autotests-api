22babfee54c7f43a2434bb1f68b130f206f46930
RyanGosling-AI <damnedwretchofficial@gmail.com>
2026-09-03 16:34:07 +1000

    Плагины Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,7 +4,7 @@ import pytest
 from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
-from conftest import UserFixture
+from fixtures.users import UserFixture
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
