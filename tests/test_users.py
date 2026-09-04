dc0513acd591bbff6077f6a81bb1a8b07ebbdead
RyanGosling-AI <damnedwretchofficial@gmail.com>
2026-09-04 10:07:35 +1000

    Параметризация Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -9,18 +9,12 @@ from tools.assertions.users import assert_create_user_response, assert_get_user_
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
-@pytest.mark.parametrize(
-    'email',
-    [
-        fake.email(domain='mail.ru'),
-        fake.email(domain='gmail.com'),
-        fake.email(domain='example.com')
-    ]
-)
+
+@pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
 @pytest.mark.users
 @pytest.mark.regression
 def test_create_user(email: str, public_users_client: PublicUsersClient):
-    request = CreateUserRequestSchema(email=email)
+    request = CreateUserRequestSchema(email=fake.email(domain=email))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
     print(response_data)
