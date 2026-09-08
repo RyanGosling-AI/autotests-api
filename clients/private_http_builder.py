6f6dd39588fa425c7e961343f49b38cd01632e9b
RyanGosling-AI <damnedwretchofficial@gmail.com>
2026-09-08 11:41:47 +1000

    Знакомство с Allure TestOPS

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -3,6 +3,8 @@ from clients.authentication.authentication_client import get_authentication_clie
 from clients.authentication.authentication_schema import LoginRequestSchema
 from pydantic import BaseModel
 from functools import lru_cache
+from clients.event_hooks import curl_event_hook
+
 
 class AuthenticationUserSchema(BaseModel, frozen=True):
     email: str
@@ -25,5 +27,6 @@ def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     return Client(
         timeout=100,
         base_url="http://localhost:8000",
-        headers={"Authorization": f"Bearer {login_response.token.access_token}"}
+        headers={"Authorization": f"Bearer {login_response.token.access_token}"},
+        event_hooks={"request": [curl_event_hook]}
     )
